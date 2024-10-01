664bd0bd30c69bc321ddb9d1eff663e27ee22c03
Lôgan <143624309+loganlaa@users.noreply.github.com>
2024-10-01 16:24:52 -0300

    Update list_objects.py
    segurança

--- s3/list_objects.py
+++ s3/list_objects.py
@@ -2,6 +2,6 @@ import boto3
 
 s3 = boto3.resource(service_name='s3', region_name='us-east-2')
 
-for obj in s3.Bucket('nergal-teste-role-endpoint').objects.all():
+for obj in s3.Bucket('NOME DA BUCKET').objects.all():
     print(obj.key)
 
